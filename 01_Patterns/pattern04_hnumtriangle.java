a622990c740baf40f98afa47d30b7dc98c6015a2
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-04 15:11:44 +0530

    added pattern04- harizontal number pattern

---
+++ 01_Patterns/pattern04_hnumtriangle.java
@@ -0,0 +1,18 @@
+public class pattern04_hnumtriangle {
+    public void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        pattern04_hnumtriangle s1=new pattern04_hnumtriangle();
+        s1.pattern(n);
+        
+    }
+}
+    
+
