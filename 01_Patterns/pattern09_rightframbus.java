621fd06d4e5563de60329e6994d06ca55f3d623d
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-04 16:08:42 +0530

    added pattern06 right half pyramid  pattern

--- 01_Patterns/pattern09_rightframbus.java
+++ 01_Patterns/pattern09_rightframbus.java
@@ -1,4 +1,4 @@
-public class pattern08_rightframbus {
+public class pattern09_rightframbus {
     public void pattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
@@ -15,7 +15,7 @@ public class pattern08_rightframbus {
     }
     public static void main(String[] args) {
         int n=5;
-        pattern08_rightframbus s1=new pattern08_rightframbus();
+        pattern09_rightframbus s1=new pattern09_rightframbus();
         s1.pattern(n);
         
     }}
