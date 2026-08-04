9b76eb7367df8c235cfb3992ae598699214cd35b
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-04 15:35:16 +0530

    added pattern06 pyramid pattern

--- 01_Patterns/pattern05_resrightangle.java
+++ 01_Patterns/pattern05_resrightangle.java
@@ -2,8 +2,8 @@ public class pattern05_resrightangle {
 
      public void pattern(int n){
         for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+            for(int j=1;j<=i;j++){                
+                System.out.print(j);           
             }
             System.out.println();
         }
@@ -14,3 +14,4 @@ public class pattern05_resrightangle {
         s1.pattern(n);
 }
 }
+
