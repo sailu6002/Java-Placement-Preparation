0c0584d910a23a2b509579740b59a23ca28a2cdf
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-04 15:50:33 +0530

    added pattern06 reverse pyramid pattern

---
+++ 01_Patterns/pattern07_respyramid.java
@@ -0,0 +1,19 @@
+public class pattern07_respyramid {
+    public void pattern(int n){
+        for(int i=n;i>=1;i--){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){                
+                System.out.print("*");           
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        pattern06_pyramid s1=new  pattern06_pyramid();
+        s1.pattern(n);
+}
+    
+}
