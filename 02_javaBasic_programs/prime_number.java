a09d6a7e1f8a376859c3d3068ff26fe0ed8e8b6b
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-06 19:17:03 +0530

    Update prime_number.java

--- 02_javaBasic_programs/prime_number.java
+++ 02_javaBasic_programs/prime_number.java
@@ -1,25 +1,22 @@
-public class prime_number {
-    public static boolean prime(int n){
-        if(n<=1){
+public class PrimeNumber {
+    public static boolean prime(int n) {
+        if (n <= 1) {
             return false;
         }
-        for(int i=2;i<n;i++){
-            if(n%i==0){
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
-    public static void main(String[] args){
-        int n=19;
-        if(prime(n)){
-            System.out.println("prime number: "+n);
-
-        }
-        else{
-            System.out.println("not prime: "+n);
+    public static void main(String[] args) {
+        int n = 19;
+        if (prime(n)) {
+            System.out.println(n + " is a Prime Number");
+        } else {
+            System.out.println(n + " is Not a Prime Number");
         }
 
     }
-    
 }
