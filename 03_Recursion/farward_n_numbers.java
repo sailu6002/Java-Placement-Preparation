a4fe9e9f864b59deb49830b57b7850069016805f
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-08 10:29:02 +0530

    added farward and backward numbers

---
+++ 03_Recursion/farward_n_numbers.java
@@ -0,0 +1,24 @@
+public class farward_n_numbers {
+    public void printName(int current, int n){
+        if(current>n){
+            return ;
+        }
+        System.out.print(current+" ");
+        printName(current+1, n);
+        
+    }
+    public void back_tracking(int current,int n){
+        if(current>n){
+            return ;
+        }
+        back_tracking(current+1, n);
+        System.out.print(current+" ");
+    }
+    public static void main(String[] args) {
+        farward_n_numbers sol=new farward_n_numbers();
+        int n=5;
+        sol.printName(1,n);
+        System.out.println();
+        sol.back_tracking(1, n);
+    }
+}
