b428196dc3f0c0d4fc95c8d5a105bbd7f620fec6
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-09 10:07:07 +0530

    addedd the largest element

---
+++ 05_Array/largest_element.java
@@ -0,0 +1,21 @@
+
+
+public class largest_element {
+    public static int largest(int[] arr,int n){
+        int max=arr[0];
+        for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,6,8,7,9,0};
+        int n=arr.length;
+        int result=largest(arr,n);
+        System.out.println(result);
+    }
+    
+    
+}
