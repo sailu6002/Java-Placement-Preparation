a220ff2e3a2d4055b5d5f9cc04c555b713dac45e
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-10 11:24:48 +0530

    addedd the ones to last

--- 05_Array/ones_right.java
+++ 05_Array/ones_right.java
@@ -1,7 +1,16 @@
 public class ones_right {
     public void onesright(int[] arr){
-        int j=0;
+        int j=-1;
         for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1){
+            return;
+        }
+        for(int i=1;i<arr.length;i++){
             if(arr[i]!=1){
                 int temp=arr[i];
                 arr[i]=arr[j];
