2e949cae0aaf2a64686a5c110e4252c7b07db6ae
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-19 15:50:23 +0530

    addedd leaders in array

--- 05_Array/Array_medium_problems/leaders.java
+++ 05_Array/Array_medium_problems/leaders.java
@@ -1,3 +1,12 @@
+/*
+1. A leader is an element that is greater than or equal to all the elements to its right.
+2. The last element is always a leader because there are no elements to its right.
+3.If the current element is greater than or equal to the current maximum, it is a leader.
+4.Update the maximum.
+5. Since leaders are found from right to left, reverse the result at the end. */
+
+
+
 import java.util.*;
 public class leaders{
     public static ArrayList<Integer> leader(int[] arr){
