30a4d22df1ed960b232cbf9d57169c1576e08fe7
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-08-31 22:09:03 +0530

    addedd the moves zeors to end

---
+++ 05_Array/two_pointers/move_zeros.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class move_zeros {
+    public static void move(int[] arr){
+        int i=0;
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=0){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
+            }
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr={0,1,2,5,0,7,0,9};
+
+        move(arr);
+        System.out.println("all zeros to the end: "+Arrays.toString(arr));
+        
+
+    }
+    
+}
