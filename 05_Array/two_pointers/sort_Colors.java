3ddbb27f865a9220f39b4301f1ceae12635cabe7
Adusuru Sailaja <aadusurusailu@gmail.com>
2026-09-01 16:46:15 +0530

    addedd the sorting colors

---
+++ 05_Array/two_pointers/sort_Colors.java
@@ -0,0 +1,38 @@
+public class sort_Colors{
+    public static void color(int[] arr){
+        int low=0;
+        int mid=0;
+        int high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp=arr[high];
+                arr[high]=arr[low];
+                arr[low]=temp;
+                low++;
+                mid++;
+
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr={2,0,1,0,1,2};
+        color(arr);
+        System.out.print("the sorted colrs is: ");
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+
+
+
+    }
+}
